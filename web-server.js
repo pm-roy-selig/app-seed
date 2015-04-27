7ea3eb166090f9e76e39738028bd2a3daa93d9ad
pm-roy-selig <roy.selig@pubmatic.com>
2015-04-27 08:31:38 -0400

    adds web-server initialization script

--- web-server.js
+++ web-server.js
@@ -4,11 +4,14 @@ var open = require('open' ),
 
 var app = express();
 
-var staticPath = path.resolve(__dirname, '/public');
+var staticPath = path.resolve(__dirname, '');
+
+console.log(staticPath);
+
 app.use(express.static(staticPath));
 
 app.listen(4040, function() {
-    console.log('listening');
+    console.log('listening on 4040');
 });
 
-open('http://localhost:4040/');
+open('http://localhost:4040/app/');
